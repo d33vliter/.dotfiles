c46fe0526572af98e8af38675b6388f598e4512a
d33vliter <iandupuy1998@gmail.com>
2023-12-03 15:34:26 -0500

    Update config.py

--- qtile/config.py
+++ qtile/config.py
@@ -3,6 +3,7 @@ from libqtile import bar, layout, widget
 from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
+from pywayland import lib
 import funcion as f
 
 mod = "mod4"
